0043a8490379ea269e50a7562ad780d4eedbc8de
Jeremi Kiermasz <kiermaszjeremi@gmail.com>
2025-11-25 13:48:16 +0000

    login validation + audit fix

--- routes/users.js
+++ routes/users.js
@@ -97,57 +97,46 @@ function logAudit(username, status) {
   });
 }
 
-router.post(
-  "/loggedin",
-  [
-    check("username").isLength({ min: 5, max: 50 }),
-    check("password").isLength({ min: 8, max: 225 }),
-  ],
-  function (req, res, next) {
-    //sanatise inputs
-    req.body.username = req.sanitize(req.body.username);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.render("./login");
-    } else {
-      let sqlquery = "SELECT * FROM users WHERE username = ?"; //get username
-      // execute sql query
-      db.query(sqlquery, [req.body.username], (err, result) => {
+router.post("/loggedin", function (req, res, next) {
+  //sanatise inputs
+  req.body.username = req.sanitize(req.body.username);
+
+  let sqlquery = "SELECT * FROM users WHERE username = ?"; //get username
+  // execute sql query
+  db.query(sqlquery, [req.body.username], (err, result) => {
+    if (err) {
+      next(err);
+    }
+    if (result.length == 0) {
+      //username doesnt exist in db
+      logAudit(req.body.username, "unknown username");
+      return res.send("Login failed: Unknown username.");
+    }
+    bcrypt.compare(
+      req.body.password,
+      result[0].hashedPassword,
+      function (err, match) {
         if (err) {
           next(err);
+        } else if (match == true) {
+          logAudit(req.body.username, "logged in");
+          // Save user session here, when login is successful
+          req.session.userId = req.body.username;
+          res.send(
+            "Welcome back " +
+              result[0].first_name +
+              " " +
+              result[0].last_name +
+              ", you are now logged in!"
+          );
+        } else {
+          logAudit(req.body.username, "incorrect password");
+          res.send("Login failed: Incorrect password.");
         }
-        if (result.length == 0) {
-          //username doesnt exist in db
-          logAudit(req.body.username, "unknown username");
-          return res.send("Login failed: Unknown username.");
-        }
-        bcrypt.compare(
-          req.body.password,
-          result[0].hashedPassword,
-          function (err, match) {
-            if (err) {
-              next(err);
-            } else if (match == true) {
-              logAudit(req.body.username, "logged in");
-              // Save user session here, when login is successful
-              req.session.userId = req.body.username;
-              res.send(
-                "Welcome back " +
-                  result[0].first_name +
-                  " " +
-                  result[0].last_name +
-                  ", you are now logged in!"
-              );
-            } else {
-              logAudit(req.body.username, "incorrect password");
-              res.send("Login failed: Incorrect password.");
-            }
-          }
-        );
-      });
-    }
-  }
-);
+      }
+    );
+  });
+});
 
 router.get("/audit", function (req, res, next) {
   let sqlquery = "SELECT * FROM auditlog ORDER BY time DESC"; //get all audit logs (list by time added)

81d43ce7bd0f3458441cf7b96fc20c36d684bc4c
Jeremi Kiermasz <kiermaszjeremi@gmail.com>
2025-11-25 13:43:08 +0000

    middleware redirect fix 2

--- middleware/redirectlogin.js
+++ middleware/redirectlogin.js
@@ -1,6 +1,6 @@
 const redirectLogin = (req, res, next) => {
   if (!req.session.userId) {
-    res.redirect("/users/login"); // redirect to the login page
+    res.redirect("/usr/148/users/login"); // redirect to the login page
   } else {
     next(); // move to the next middleware function
   }
